c051a64a22381bcbe779cc4cd0ad4b4ff2403051
DanielIppo <91868349+DanielIppo@users.noreply.github.com>
2022-03-17 10:31:51 +0200

    Update Algoritmo Cerca Array.cpp

--- Algoritmo Cerca Array/Algoritmo Cerca Array/Algoritmo Cerca Array.cpp
+++ Algoritmo Cerca Array/Algoritmo Cerca Array/Algoritmo Cerca Array.cpp
@@ -3,23 +3,30 @@
 using namespace std;
 
 int main() {
-	int numeroDaTrovare = 44, indice;
-	int a [] = {1,4,5,19,32,33,42,44,51,60,66,69,87,90,91,99,100};
-
+	int numeroDaTrovare, indice = -1;
+	int a [] = {1,4,5,19,32,33,42,44,51,60,66,69,87,90,91,99,100, 1001};
+    
+    cout<<"Inserisci un numero da trovare: ";
+    cin>>numeroDaTrovare;
+    
 	int fine = sizeof(a)/sizeof(a[0]);
-	for (int inizio = 0; inizio < fine; ) {
+	int inizio = 0;
+	while(inizio <= fine){
 		int medio = (inizio + fine) / 2;
-		if (a[medio] == numeroDaTrovare || a[inizio] == numeroDaTrovare || a[fine] == numeroDaTrovare) {
+		if (a[medio] == numeroDaTrovare ) {
 			indice = medio;
 			cout << "Numero trovato a indice: " << indice;
 			break;
 		}
 		if (a[medio] < numeroDaTrovare) {
-			inizio = medio + 1;
+			inizio = medio  + 1;
 		}
 		else 
 		{
 			fine = medio - 1;
 		}
+	}
+	if(indice ==-1){
+	    cout << "Non trovato";
 	}
 }
